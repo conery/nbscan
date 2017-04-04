e61e530b8825d31210bdfe783de61125f331c637
John Conery <conery@uoregon.edu>
2017-04-04 09:00:28 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'nbscan',
+    version = '1.0',
+    scripts = ['nbscan.py'],
+    author = 'John Conery',
+    author_email = 'conery@uoregon.edu',
+    url = 'https://github.com/conery/nbscan'
+)
